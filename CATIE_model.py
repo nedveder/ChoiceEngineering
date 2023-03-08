fe6fd7456b24684bb7ce1f9891b08be93daa3ae3
nedved <nadavleder@gmail.com>
2023-03-08 15:08:38 +0200

    transition to different representation of choices

--- CATIE_model.py
+++ CATIE_model.py
@@ -4,17 +4,14 @@ TAO = 0.29
 EPSILON = 0.30
 PHI = 0.71
 K = 2
-OPTIONS = ["A", "B"]
 ALTERNATIVE_A = 1
 ALTERNATIVE_B = 0
-REWARD = 1
-NO_REWARD = 0
 
 
-class CatieModel:
-    choice_history = ""  # String containing choices "A" for alternative 1 or "B" for alternative 2
-    reward_history = ""  # String containing rewards observed 1 if seen else 0
-    choices_allocations = []
+class CatieAgent:
+    previous_choices = []
+    alt_a_rewards = []
+    alt_b_rewards = []
     trial_num = 0
     p_explore = 0
     k = 0
@@ -24,140 +21,190 @@ class CatieModel:
     all_surprise = []
 
     def __init__(self):
-        self.choice_history = ""  # String containing choices "A" for alternative 1 or "B" for alternative 2
-        self.reward_history = ""  # String containing rewards observed 1 if seen else 0
-        self.choices_allocations = []
-        self.trial_num = 0
+        # List of previous choices, where 1's indicate a
+        # choice of alternative A and 0's indicate choice of alternative B.
+        self.previous_choices = []
+        # List of observed rewards for Alternative A on each trial
+        self.alt_a_rewards = []
+        # List of observed rewards for Alternative B on each trial
+        self.alt_b_rewards = []
         self.p_explore = 0
         self.ca_alt_1 = 0
         self.ca_alt_2 = 0
         self.all_surprise = []
-        self.reward_history_dict = {"A": [], "B": []}
-        self.k = np.random.randint(0, K + 1)
+        self.k = np.random.randint(0, K+1)
         self.choice_type_history = ""
 
-    def choose(self, reward_func):
-        choice = ""
+    def set_reward(self, reward):
+        self.alt_a_rewards.append(reward[0])
+        self.alt_b_rewards.append(reward[1])
 
-        # INIT
-        if self.trial_num < 2:
-            # Samples both alternatives with random order
-            choice = self.random_choice() if self.trial_num == 0 else self.other_choice()
-            self.choice_history += choice
-            reward = reward_func(self.trial_num, choice)
-            self.reward_history_dict[choice].append(reward)
-            self.reward_history += str(reward)
-            self.trial_num += 1
-            self.choice_type_history += "0"
-            self.calculate_p_explore()
-            return choice
-
-        self.calculate_p_explore()
+    def choose(self):
+        """
+        Controls choice mechanism for CATIE agent, using the 4 different modes for decision-making.
+            1.If the agent chose the same alternative in two consecutive trials and their outcomes differed, it
+            would choose in the subsequent trial the trend (or heuristic) mode with a probability 𝜏.
+            2. If the trend mode was not chosen, the agent would choose the explore mode with a probability p_explore.
+            3. If neither trend nor explore modes were chosen,the agent would choose, the inertia mode with
+             a probability 𝜙.
+            4. If none of the above modes was chosen, the agent would choose the contingent average mode.
+        Adds the current choice made to the prev_choice list and returns the choice to caller.
+        The method updates a list containing the different modes used for every decision.
+        """
+        # Calculate Contingent Average and p_explore parameters.
         self.ca_alt_1, self.ca_alt_2 = self.contingent_average()
+        self.p_explore = self.calculate_p_explore()
 
-        # TREND
-        if self.choice_history[self.trial_num - 1] == self.choice_history[self.trial_num - 2] \
-                and self.reward_history[self.trial_num - 1] != self.reward_history[self.trial_num - 2] \
+        # INIT MODE
+        # For the first two trials we sample both options in random order.
+        if len(self.previous_choices) < 2:
+            # Samples both alternatives with random order
+            choice = self.random_choice() if len(self.previous_choices) == 0 else self.other_choice()
+            self.choice_type_history += "0"
+
+        # TREND MODE
+        # If we chose the same alternative in the past two trials and received different outcomes then with probability
+        # TAO we enter the TREND mode.
+        elif self.previous_choices[len(self.previous_choices) - 1] == self.previous_choices[len(self.previous_choices) - 2] \
+                and (self.alt_a_rewards[len(self.previous_choices) - 1] != self.alt_a_rewards[len(self.previous_choices) - 2]
+                     or self.alt_b_rewards[len(self.previous_choices) - 1] != self.alt_b_rewards[len(self.previous_choices) - 2]) \
                 and np.random.random() <= TAO:
             choice = self.trend()
             self.choice_type_history += "T"
 
-        # EXPLORE
+        # EXPLORE MODE
+        # Explore is entered with probability p_explore which is determined using calculate_p_explore method at
+        # each trial. Explore mode means choosing at random from both alternatives.
         elif np.random.random() <= self.p_explore:
-            choice = self.explore()
+            choice = self.random_choice()
             self.choice_type_history += "E"
 
-        # INERTIA
+        # INERTIA MODE
+        # Choose the previous choice again.
         elif np.random.random() <= PHI:
-            choice = self.inertia()
+            choice = self.previous_choices[-1]
             self.choice_type_history += "I"
 
-        # CONTINGENT AVERAGE
+        # CONTINGENT AVERAGE MODE
+        # In the contingent average (CA) mode, the agent chooses the alternative associated with the higher k-CA,
+        # defined as the average payoff observed in all previous trials which followed the same sequence of k outcomes
+        # See contingent_average method for further information.
         else:
             if self.ca_alt_1 == self.ca_alt_2:
                 choice = self.random_choice()
             else:
-                choice = OPTIONS[0] if self.ca_alt_1 > self.ca_alt_2 else OPTIONS[1]
+                choice = ALTERNATIVE_A if self.ca_alt_1 > self.ca_alt_2 else ALTERNATIVE_B
             self.choice_type_history += "A"
 
-        self.choice_history += choice
-        reward = reward_func(self.trial_num, choice)
-        self.reward_history_dict[choice].append(reward)
-        self.reward_history += str(reward)
-        self.trial_num += 1
+        self.previous_choices.append(choice)
         return choice
 
     @staticmethod
     def random_choice():
-        return OPTIONS[np.random.randint(0, 2)]
+        """
+        :return: A random choice between the two alternatives.
+        """
+        return np.random.choice([ALTERNATIVE_A, ALTERNATIVE_B])
 
     def other_choice(self):
-        return OPTIONS[(OPTIONS.index(self.choice_history[self.trial_num - 1]) + 1) % 2]
+        """
+        :return: Given the previous choice return the other alternative.
+        """
+        return (self.previous_choices[-1] + 1) % 2
 
     def trend(self):
-        return self.choice_history[self.trial_num - 1] \
-            if self.reward_history[self.trial_num - 1] > self.reward_history[self.trial_num - 2] \
-            else self.other_choice()
-
-    def explore(self):
-        return self.random_choice()
-
-    def inertia(self):
-        return self.choice_history[self.trial_num - 1]
+        """
+        In this mode the agent chooses the “positive trend” meaning if the outcome of the previous choice was
+        bigger then the one preceding it, we choose it ̧otherwise we switch choices.
+        :return: The choice corresponding with TREND mode.
+        """
+        matching_choice_rewards = self.alt_a_rewards if self.previous_choices[-1] == 1 else self.alt_b_rewards
+        if matching_choice_rewards[- 1] > matching_choice_rewards[- 2]:
+            return self.previous_choices[-1]
+        else:
+            return self.other_choice()
 
     @staticmethod
-    def overlapping_substring(string, pattern):
-        return [i for i in range(len(string) - len(pattern) + 1) if string[i: i + len(pattern)] == pattern]
+    def overlapping_sublist(lst, sub_lst):
+        """
+        Using a sliding window across the list we get all occurrences of sub list in list
+        :return: A list of indexes for the start of each time the sub_lst occurred.
+        """
+        return [i for i in range(len(lst) - len(sub_lst) + 1) if lst[i: i + len(sub_lst)] == sub_lst]
 
     def contingent_average(self):
-        sum_alt_1, count_alt_1, sum_alt_2, count_alt_2 = 0, 0, 0, 0
-
+        alt_a_contingency_rewards = []
+        alt_b_contingency_rewards = []
+        if len(self.previous_choices) < 2:
+            return 0, 0
         if self.k == 0:
-            k_contingencies = list(range(0, self.trial_num - 1))
+            k_contingencies = list(range(0, len(self.previous_choices) - 1))
         else:
-            k_contingencies = self.overlapping_substring(self.choice_history, self.choice_history[-self.k:])
+            k_contingency = self.previous_choices[-self.k:]
+            k_contingencies = self.overlapping_sublist(self.previous_choices, k_contingency)
 
         for trial in k_contingencies:
-            if trial + self.k == self.trial_num:
-                break
-            if self.choice_history[trial + self.k] == OPTIONS[0]:
-                sum_alt_1 += int(self.reward_history[trial + self.k])
-                count_alt_1 += 1
-            else:
-                sum_alt_2 += int(self.reward_history[trial + self.k])
-                count_alt_2 += 1
+            # Checks whether this is the last k_contingency meaning there is no element afterwards,
+            # so we can't check if we received a reward , in such a case we skip.
+            if trial + self.k >= len(self.previous_choices):
+                continue
+            # If the next choice was Alternative A
+            elif self.previous_choices[trial + self.k] == ALTERNATIVE_A:
+                alt_a_contingency_rewards.append(self.alt_a_rewards[trial + self.k])
+            # otherwise the next choice was Alternative B
+            elif self.previous_choices[trial + self.k] == ALTERNATIVE_B:
+                alt_b_contingency_rewards.append(self.alt_b_rewards[trial + self.k])
 
-        if count_alt_1 == 0:
-            count_alt_1, sum_alt_1 = self.no_k_contingency(OPTIONS[0])
-        if count_alt_2 == 0:
-            count_alt_2, sum_alt_2 = self.no_k_contingency(OPTIONS[1])
-        return (sum_alt_1 / count_alt_1), (sum_alt_2 / count_alt_2)
+        if len(alt_a_contingency_rewards) == 0:
+            alt_a_contingency_rewards = self.no_k_contingency(ALTERNATIVE_A)
+        if len(alt_b_contingency_rewards) == 0:
+            alt_b_contingency_rewards = self.no_k_contingency(ALTERNATIVE_B)
+
+        return np.mean(alt_a_contingency_rewards), np.mean(alt_b_contingency_rewards)
 
     def no_k_contingency(self, alternative):
+        """
+        If for alternative 𝑖 there are no past k-contingencies matching the
+        most recent contingency, the value of a different random k-
+        contingency replaces the contingent average for that
+        alternative. A random k-contingency value is obtained by
+        considering the set of all reward sequences of length 𝑘 which
+        were followed by choice of 𝑖, randomly choosing one of them
+        with a uniform probability and calculating its contingent
+        average value. Note that choosing uniformly from past trials
+        implies that recurring contingencies have higher probability of
+        being chosen. If there are no contingencies of length 𝑘 that are
+        followed by a choice of 𝑖, a smaller k (k:=k-1) is chosen
+        iteratively until at least one contingency exists.
+        :param alternative: 0 or 1
+        :return: list of current alternative contingencies
+        """
         cur_k = self.k
-        sum_alt, count_alt = 0, 0
-        while count_alt == 0:
+        alt_contingency_rewards = []
+        alt_rewards = self.alt_a_rewards if self.previous_choices[-1] == 1 else self.alt_b_rewards
+        while len(alt_contingency_rewards) == 0:
             if cur_k == 0:
-                optional_k_contingencies = list(range(0, self.trial_num - 1))
+                optional_k_contingencies = list(range(len(self.previous_choices)))
             else:
-                optional_k_contingencies = self.overlapping_substring(self.choice_history[:-cur_k], alternative)
+                optional_k_contingencies = self.overlapping_sublist(self.previous_choices[:-cur_k], [alternative])
 
             if optional_k_contingencies:
                 index = np.random.choice(optional_k_contingencies)
-                k_contingencies = self.overlapping_substring(self.choice_history[cur_k:],
-                                                             self.choice_history[index:index + cur_k] + alternative)
+                k_contingencies = self.overlapping_sublist(self.previous_choices[cur_k:],
+                                                           self.previous_choices[index:index + cur_k] + [alternative])
                 for trial in k_contingencies:
-                    sum_alt += int(self.reward_history[trial + cur_k - 1])
-                    count_alt += 1
+                    alt_contingency_rewards.append(alt_rewards[trial + cur_k])
             cur_k -= 1
-        return count_alt, sum_alt
+        return alt_contingency_rewards
 
     def calculate_p_explore(self):
-        obs_sd = np.std(self.reward_history_dict[self.choice_history[-1]])
+        if len(self.previous_choices) == 0:
+            return 0
+        cur_alt_rewards = self.alt_a_rewards if self.previous_choices[-1] else self.alt_b_rewards
+        obs_sd = np.std(cur_alt_rewards)
         surprise_t = 0
         if obs_sd > 0:
-            x = abs((self.ca_alt_1 if self.choice_history[-1] == OPTIONS[0] else self.ca_alt_2)
-                    - int(self.reward_history[-1]))
+            x = abs((self.ca_alt_1 if self.previous_choices[-1] else self.ca_alt_2) - cur_alt_rewards[-1])
             surprise_t = x / (obs_sd + x)
         self.all_surprise.append(surprise_t)
-        self.p_explore = EPSILON * (1 + surprise_t + np.mean(self.all_surprise)) / 3
+        return EPSILON * (1 + surprise_t + np.mean(self.all_surprise)) / 3
--- main.py
+++ main.py
@@ -9,26 +9,25 @@ TRIALS = 100
 BASIC_SCHEDULE = "1111111111111111111111111000000000000000000000000000000000000000000000000002222222222222222222222222"
 
 
-def change_schedule(schedule=BASIC_SCHEDULE):
-    def reward_func(trial_num, choice):
-        if schedule[trial_num] == "0":
-            return 0
-        if schedule[trial_num] == "2" and choice == CATIE_model.OPTIONS[0]:
-            return 0
-        if schedule[trial_num] == "1" and choice == CATIE_model.OPTIONS[1]:
-            return 0
-        return 1
-
-    return reward_func
+def reward_func(trial_num, schedule, choice):
+    biased_alt, non_biased_alt = 0, 0
+    if schedule[trial_num] == "0":
+        return 0, 0
+    if (schedule[trial_num] == "1" or schedule[trial_num] == "3") and choice == CATIE_model.ALTERNATIVE_A:
+        biased_alt = 1
+    if (schedule[trial_num] == "2" or schedule[trial_num] == "3") and choice == CATIE_model.ALTERNATIVE_B:
+        non_biased_alt = 1
+    return biased_alt, non_biased_alt
 
 
 def main():
     iterations = []
     for _ in tqdm(range(ITERATIONS)):
-        ca_model = CATIE_model.CatieModel()
-        for __ in range(TRIALS):
-            ca_model.choose(change_schedule())
-        iterations.append(ca_model.choice_history.count("A") / TRIALS)
+        ca_model = CATIE_model.CatieAgent()
+        for i in range(TRIALS):
+            choice = ca_model.choose()
+            ca_model.set_reward(reward_func(i, BASIC_SCHEDULE, choice))
+        iterations.append(ca_model.previous_choices.count(CATIE_model.ALTERNATIVE_A) / TRIALS)
 
     n, bins, patches = plt.hist(iterations, 50, density=True, facecolor='g', alpha=0.75)
     plt.axvline(np.array(iterations).mean(), color='k', linestyle='dashed', linewidth=1)
