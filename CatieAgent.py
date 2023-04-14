Working training loop, fixed gradient issues

CatieAgent.py
@@ -1,6 +1,5 @@
 import random
 import statistics
-
 import numpy as np
 import tqdm
 from matplotlib import pyplot as plt
@@ -155,19 +154,13 @@ class CatieAgent:
         # Current contingency does not exist
         if not choice_contingencies:
             # If there are no k-length contingencies simply consider the mean
-            return [np.mean(choice_outcomes[:self.choice_indexes[choice]])] if self.choice_indexes[choice] else 0
+            return np.mean(choice_outcomes[:self.choice_indexes[choice]]) if self.choice_indexes[choice] else 0
         else:
             # The current contingency does not appear, but other contingencies do. In such case the user gets
             # "confused" and chooses one of the other contingencies at random. When contingencies seen more will have a
             # higher outcome probability.
             all_contingencies = list(choice_contingencies.values())
-            total_contingencies = sum(i for (i, r) in choice_contingencies.values())
-            random_contingency_index = random.randint(0, total_contingencies)
-            i = 0
-            while all_contingencies[i][0] < random_contingency_index:
-                random_contingency_index -= all_contingencies[i][0]
-                i += 1
-            random_contingency = all_contingencies[i]
+            random_contingency = all_contingencies[np.random.randint(0, len(all_contingencies))]
             index = random_contingency[CONTINGENCIES_INDEX]
             contingency_values = random_contingency[CONTINGENCIES_ARRAY]
             return np.mean(contingency_values[:index])
@@ -235,6 +228,9 @@ class CatieAgent:
         """
         return self.alt_a_contingent_average, self.alt_b_contingent_average
 
+    def get_bias(self):
+        return self.previous_choices.sum() / 100
+
 
 def sequence_catie_score(reward_schedule, repetitions=100, plot_distribution=False, plot_sequence=False):
     schedule_target, schedule_anti_target = reward_schedule[0], reward_schedule[1]
@@ -246,7 +242,8 @@ def sequence_catie_score(reward_schedule, repetitions=100, plot_distribution=Fal
             choice = catie_agent.choose()
             outcome = reward_target, reward_anti_target
             catie_agent.receive_outcome(choice, outcome)
-            choices.append(choice)
+            if t == 99:
+                choices.append(choice)
         biases.append(sum(choices))
 
     if plot_sequence:

CatieAgent_Ohad_Imp.py
@@ -219,13 +219,15 @@ class CatieAgent:
             probability_of_entering_ca_mode = probability_of_trying_inertia_mode * (1 - self.phi)
             if not self.outcomes_biased or not self.outcomes_anti_biased:  # If one of the alternatives was not sampled yet
                 # Then "exploit" means continue choosing the already chosen alternative
-                contingency_mode_biased_probability = probability_of_entering_ca_mode if self.outcomes_biased else 1-probability_of_entering_ca_mode
+                contingency_mode_biased_probability = probability_of_entering_ca_mode if self.outcomes_biased else 1 - probability_of_entering_ca_mode
             else:
                 if self.k > 0:
                     all_contingencies_probabilities = [biased_side_p_for_ca(ca_biased, ca_anti_biased)
                                                        for ca_biased in self.__contingent_average(self.k, CHOICE_BIASED)
-                                                       for ca_anti_biased in self.__contingent_average(self.k, CHOICE_ANTI_BIASED)]
-                    contingency_mode_biased_probability = probability_of_entering_ca_mode * np.mean(all_contingencies_probabilities)
+                                                       for ca_anti_biased in
+                                                       self.__contingent_average(self.k, CHOICE_ANTI_BIASED)]
+                    contingency_mode_biased_probability = probability_of_entering_ca_mode * np.mean(
+                        all_contingencies_probabilities)
                 else:
                     target_mean, anti_target_mean = np.mean(self.outcomes_biased), np.mean(self.outcomes_anti_biased)
                     if target_mean == anti_target_mean:  # If the two alternative means are equal, choose randomly
@@ -269,7 +271,8 @@ class CatieAgent:
         if choice == CHOICE_BIASED:
             choice_probability = self.target_side_choice_probability
             self.outcomes_biased.append(outcome)
-            obs_sd = 0 if len(self.outcomes_biased) < 2 else np.std(self.outcomes_biased)  # stddv is only defined for 2 or more numbers
+            obs_sd = 0 if len(self.outcomes_biased) < 2 else np.std(
+                self.outcomes_biased)  # stddv is only defined for 2 or more numbers
         elif choice == CHOICE_ANTI_BIASED:
             choice_probability = 1 - self.target_side_choice_probability
             self.outcomes_anti_biased.append(outcome)
@@ -326,7 +329,8 @@ def sequence_catie_score(reward_schedule,
             choice = catie_agent.choose()
             outcome = reward_targe if choice == CHOICE_BIASED else reward_anti_target
             catie_agent.receive_outcome(choice, outcome)
-            choices.append(choice)
+            if t == 99:
+                choices.append(choice)
         biases.append(sum(choices))
     if plot_sequence:
         plt.plot([i + 1 for i in range(100) if reward_schedule[0][i]], 2 * np.ones(25), 'x')

Reinforcment Deep Learning/Engineer.py
@@ -7,7 +7,8 @@ from gymnasium import spaces
 import numpy as np
 from CatieAgent import CatieAgent
 import matplotlib.pyplot as plt
-from IPython.display import clear_output
+import pickle
+import torch.nn.functional as F
 
 ALLOCATION_DICT = {0: (0, 0), 1: (1, 0), 2: (0, 1), 3: (1, 1)}
 DEALLOCATION_DICT = {(0, 0): 0, (1, 0): 1, (0, 1): 2, (1, 1): 3}
@@ -87,7 +88,8 @@ class Agent:
         action_probs = self.policy_net(state_tensor)
         action_probs = nn.functional.softmax(action_probs, dim=-1)
         # Create a mask for valid actions
-        mask = torch.tensor([1.0, 1.0, 1.0, 1.0])
+        mask = torch.FloatTensor([1.0, 1.0, 1.0, 1.0])
+        add_mask = torch.FloatTensor([1e-8, 0.0, 0.0, 0.0])
         if assignments[0] >= 25:
             mask[1] = 0
             mask[3] = 0
@@ -96,57 +98,76 @@ class Agent:
             mask[3] = 0
 
         # Apply the mask to the action probabilities
-        constrained_probs = action_probs * mask
-        constrained_probs = constrained_probs / constrained_probs.sum()
+        constrained_probs = (action_probs + add_mask) * mask
+        constrained_probs = constrained_probs / (constrained_probs.sum())
         return constrained_probs
 
-    def train(self, num_episodes):
+    def train(self, num_episodes, n_rep):
         plt.figure(figsize=(10, 5))
         episode_rewards = []
+        rep_rewards = []
+        rep_per_episode = np.arange(n_rep, num_episodes)
+        top_bias = 0
         for i in range(num_episodes):
-            state, _ = self.env.reset()
-            episode_reward = 0
             action_probs = []
-            actions = []
-            for _ in range(100):
-                action_prob = self.select_action(state)
-                action_probs.append(action_prob)
-                action = ALLOCATION_DICT[torch.multinomial(action_prob, num_samples=1).item()]
-                actions.append(action)
+            for _ in range(rep_per_episode[i]):
+                episode_reward = 0
+                actions = []
+                rep_rewards = []
+                state, _ = self.env.reset()
+                action_probs_rep = []
+                for _ in range(100):
+                    action_prob = self.select_action(state)
+                    action_probs_rep.append(action_prob)
+                    action = ALLOCATION_DICT[torch.multinomial(action_prob, num_samples=1).item()]
+                    actions.append(action)
 
-                next_state, reward, done, _ = self.env.step(action)
-                episode_reward += reward
+                    next_state, reward, done, _ = self.env.step(action)
+                    episode_reward += reward
 
-                if done:
-                    break
+                    if done:
+                        break
 
-                state = next_state
+                    state = next_state
 
-            episode_rewards.append(episode_reward)
+                action_probs.append(
+                    torch.stack(action_probs_rep + [torch.mean(torch.stack(action_probs_rep), dim=0) for _ in
+                                                    range(100 - len(action_probs_rep))]))
+                rep_rewards.append(episode_reward)
+
+            episode_rewards.append(np.mean(rep_rewards))
             # Calculate the episode return (discounted sum of rewards)
-            G = torch.Tensor([episode_reward])
-
+            G = torch.Tensor([np.mean(rep_rewards)])
+            padded_action_probs = torch.stack(
+                action_probs + [torch.mean(torch.stack(action_probs), dim=0) for _ in range(100 - len(action_probs))])
+            action_probs_mean = torch.mean(padded_action_probs, dim=0)
             # Update the policy network
             self.optimizer.zero_grad()
 
-            loss = -G * sum(
-                [torch.log(t_probs[DEALLOCATION_DICT[actions[t]]] + 1e-6) for t, t_probs in enumerate(action_probs)])
+            # Add the MSE loss term to penalize the difference between G and the target value
+            mse_loss = F.mse_loss(G, torch.tensor([100], dtype=torch.float32))
+
+            loss = -mse_loss * torch.sum(torch.log(action_probs_mean + 1e-6) * 0.01)
+            ##
+            if np.mean(rep_rewards) > top_bias:
+                top_bias = np.mean(rep_rewards)
+                print(f"Top Network:{top_bias} Loss:{loss} Iterations:{rep_per_episode[i]}")
+                with open('policy_net.pkl', 'wb') as file:
+                    torch.save(self.policy_net.state_dict(), file)
+            ##
             loss.backward()
             torch.nn.utils.clip_grad_norm_(self.policy_net.parameters(), 1.0)
             self.optimizer.step()
 
-            # Update the live plot
-            clear_output(wait=True)
-
-            plt.plot(episode_rewards)
-            plt.xlabel('Episode')
-            plt.ylabel('Reward')
-            plt.title('Training Rewards')
-            plt.show()
-
             # Print progress
-            if i % 10 == 0:
-                print('Episode %d: reward = %d' % (i, episode_reward))
+            if i % 100 == 0:
+                print('Episode %d: reward = %d' % (i, episode_rewards[i]), loss)
+                # Update the live plot
+                plt.plot(episode_rewards)
+                plt.xlabel('Episode')
+                plt.ylabel('Reward')
+                plt.title('Training Rewards')
+                plt.show()
 
 
 if __name__ == '__main__':
@@ -161,12 +182,10 @@ if __name__ == '__main__':
     output_size = 4
     lr = 0.001
     num_episodes = 100000
+    n_repetitions = 10
 
     # Create the environment and agent
     agent = Agent(env, input_size, hidden_size, output_size, lr)
 
     # Train the agent
-    agent.train(num_episodes)
-
-    # Save the trained model
-    torch.save(agent.policy_net.state_dict(), 'trained_model.pth')
+    agent.train(num_episodes, n_repetitions)

Scheduler.py
@@ -12,7 +12,7 @@ NUMBER_OF_TRIALS = 100
 # Network constants
 L0_SIZE = 9  # Number of input neurons (reflecting current state, don't change)
 L_MIDDLE_SIZE = 100  # Number of neurons per each of the middle layers (arbitrary, can be changed)
-N_MIDDLE_LAYERS = 2  # Arbitrary, can be changed
+N_MIDDLE_LAYERS = 4  # Arbitrary, can be changed
 # Number of neurons in the output layer, corresponding to the number of possible decisions (don't change)
 L_END_SIZE = 4
 
@@ -69,7 +69,7 @@ def init_nn(n_middle_layers=N_MIDDLE_LAYERS, l_middle_size=L_MIDDLE_SIZE):
 NEURON_TO_ACTION = {0: (0, 0), 1: (1, 0), 2: (0, 1), 3: (1, 1)}
 
 
-def nn_action_selection_for_input(nn, input_activation):
+def nn_action_selection_for_input(nn: np.ndarray, input_activation: np.ndarray):
     activation = input_activation
     for layer in nn:
         activation = np.tanh(np.matmul(activation, layer))
@@ -88,8 +88,9 @@ def get_catie_param(catie_agent):
 def get_input_activation(trial_number=0, allocated_reward_target=0,
                          allocated_reward_anti_target=0, catie_agent=CatieAgent(NUMBER_OF_TRIALS)):
     intercept = [1]
-    return np.concatenate((intercept, [trial_number, allocated_reward_target, allocated_reward_anti_target],
-                           get_catie_param(catie_agent)))
+    a = np.concatenate((intercept, [trial_number, allocated_reward_target, allocated_reward_anti_target],
+                        get_catie_param(catie_agent)))
+    return a
 
 
 def constrain_one_side(allocation, r_remain, trial_number):
@@ -137,7 +138,7 @@ def network_multiple_runs(nn, n_runs=10):
     return biases, np.mean(biases)
 
 
-MUTATION_BASE_RATE = 0.05  # Arbitrary, greater numbers induce greater mutation for each generation
+MUTATION_BASE_RATE = 0.005  # Arbitrary, greater numbers induce greater mutation for each generation
 
 
 def mutate(layer, noise_magnitude=MUTATION_BASE_RATE):
@@ -148,7 +149,7 @@ def mutate(layer, noise_magnitude=MUTATION_BASE_RATE):
     return layer + noise
 
 
-def crossover_and_mutate(nn1, nn2, progress):
+def crossover_and_mutate(nn1, nn2, progress, mutate_rate=MUTATION_BASE_RATE):
     """
     Return the outcome of crossing (choosing weights randomly from each nn) and
     mutating the outcome (just inserting noise).
@@ -159,13 +160,13 @@ def crossover_and_mutate(nn1, nn2, progress):
         indices_from_n1 = np.random.randint(low=0, high=2, size=np.shape(layer_i_1))
         crossover_layer_i = np.where(indices_from_n1, layer_i_1, layer_i_2)
         # Mutate
-        mutation_magnitude = MUTATION_BASE_RATE * (1 - progress)
+        mutation_magnitude = mutate_rate * (1 - progress)
         crossover_mutated_output_layer_i = mutate(crossover_layer_i, mutation_magnitude)
         output_nn.append(crossover_mutated_output_layer_i)
     return output_nn
 
 
-def get_next_generation(nn1, nn2, generation_size, progress):
+def get_next_generation(nn1, nn2, generation_size, progress, mutate_rate):
     """
     Next generation is made of:
       1. One new random set of weights
@@ -177,11 +178,12 @@ def get_next_generation(nn1, nn2, generation_size, progress):
       progress is a variable between 0 and 1 representing what proportion of the
       optimization has been completed (0 indicates just started, 1 means ended)
     """
-    return [init_nn(), nn1, nn2] + [crossover_and_mutate(nn1, nn2, progress) for _ in range(generation_size - 3)]
+    return [init_nn(), nn1, nn2] + [crossover_and_mutate(nn1, nn2, progress, mutate_rate) for _ in
+                                    range(generation_size - 3)]
 
 
 # Where should the data be saved
-NETWORK_NAME = '6 layers network'
+NETWORK_NAME = '6 layers network different mutation rate'
 OUTPUT_PATH = "Data/" + NETWORK_NAME.replace(" ", "_") + ".pickle"
 
 
@@ -198,7 +200,7 @@ def load_network():
 
 
 TEST_REPETITION = 1000
-DEBUG = True
+DEBUG = False
 if DEBUG:
     MIN_OPTIMIZATION_PER_NN = 2
     MAX_OPTIMIZATION_PER_NN = 5
@@ -206,9 +208,9 @@ if DEBUG:
     NUM_OPTIMIZATION_TRIALS = 4
 else:
     MIN_OPTIMIZATION_PER_NN = 100
-    MAX_OPTIMIZATION_PER_NN = 900
+    MAX_OPTIMIZATION_PER_NN = 1000
     GENERATION_SIZE = 10
-    NUM_OPTIMIZATION_TRIALS = 800
+    NUM_OPTIMIZATION_TRIALS = 1000
 
 OPTIMIZATION_PER_REPETITION = np.linspace(MIN_OPTIMIZATION_PER_NN, MAX_OPTIMIZATION_PER_NN, num=NUM_OPTIMIZATION_TRIALS)
 
@@ -217,7 +219,7 @@ def get_two_best_networks(networks_scores):
     return heapq.nlargest(2, range(len(networks_scores)), key=lambda x: networks_scores[x][1])
 
 
-def run_optimization(generation, iteration_start, generation_mean_bias=None):
+def run_optimization(mutate_rate, generation, iteration_start, generation_mean_bias=None):
     t = tqdm.trange(iteration_start, NUM_OPTIMIZATION_TRIALS, desc="Iteration")
     best_nn = None
 
@@ -238,22 +240,22 @@ def run_optimization(generation, iteration_start, generation_mean_bias=None):
                           refresh=True)
         generation_mean_bias[1][i] = bias_distribution_nn_mean
         save_network(best_nn, i, optimization_trials, generation_mean_bias)
-        generation = get_next_generation(generation[best_two_nn_indices[0]],
-                                         generation[best_two_nn_indices[1]],
-                                         GENERATION_SIZE,
-                                         i / NUM_OPTIMIZATION_TRIALS)
+        generation = get_next_generation(nn1=generation[best_two_nn_indices[0]],
+                                         nn2=generation[best_two_nn_indices[1]],
+                                         generation_size=GENERATION_SIZE,
+                                         progress=i / NUM_OPTIMIZATION_TRIALS, mutate_rate=mutate_rate)
     return best_nn, generation_mean_bias
 
 
-def continue_optimization():
+def continue_optimization(mutate_rate):
     best_nn, iteration, generation_mean_bias = load_network()
     generation = [best_nn] + [init_nn() for _ in range(GENERATION_SIZE - 1)]
-    return run_optimization(generation, iteration)
+    return run_optimization(mutate_rate, generation, iteration)
 
 
-def run_optimization_from_scratch():
+def run_optimization_from_scratch(mutate_rate):
     generation = [init_nn() for _ in range(GENERATION_SIZE)]
-    return run_optimization(generation, 0)
+    return run_optimization(mutate_rate, generation, 0)
 
 
 def plot_distribution(dist, color, dist_type):
@@ -280,33 +282,40 @@ def get_static_catie_opt():
 
 
 def main():
-    best_nn, generation_mean_bias = run_optimization_from_scratch()
-    # best_nn, generation_mean_bias = continue_optimization()
+    fig, ax = plt.subplots()
+    color_sequence = ['#ff7f0e']
+    mutation_rates = [0.005]
+    for i, mutate_rate in enumerate(mutation_rates):
+        # best_nn, generation_mean_bias = run_optimization_from_scratch(mutate_rate)
+        best_nn, generation_mean_bias = continue_optimization(mutate_rate)
+        bias_distribution_best_nn, mean_bias_best_nn = network_multiple_runs(best_nn, TEST_REPETITION)
+        ax.plot(generation_mean_bias[0], generation_mean_bias[1], color=color_sequence[i])
+        mutation_rates[i] = (mean_bias_best_nn,
+                             f'error: +/-{statistics.pstdev(bias_distribution_best_nn) / TEST_REPETITION:.3f}%')
+    ax.legend(mutation_rates)
+    ax.set(xlabel='Generation', ylabel='Mean Generation Bias (%)')
+    ax.grid()
+    fig.show()
     plt.figure()
-    bias_distribution_best_nn, mean_bias_best_nn = network_multiple_runs(best_nn, TEST_REPETITION)
+    bias_distribution_naive_optimal_static = plot_schedules(bias_distribution_best_nn)
+    plt.show()
+
+
+def plot_schedules(bias_distribution_best_nn):
     # Plot Dynamic schedule histogram
     dist, dist_mean = plot_distribution(bias_distribution_best_nn, [x / 255 for x in (102, 196, 197)], 'Dynamic')
     plt.text(statistics.mean(dist) * 1.1, plt.ylim()[1] * 0.9, 'mean: {:.2f}'.format(dist_mean))
-
     # Plot static schedule histogram
     bias_distribution_naive_optimal_static, bias_distribution_naive_optimal_static_mean = get_static_catie_opt()
     dist, dist_mean = plot_distribution(bias_distribution_naive_optimal_static, [x / 255 for x in (238, 50, 51)],
                                         'Static')
     plt.text(statistics.mean(dist) * 1.1, plt.ylim()[1] * 0.8, 'mean: {:.2f}'.format(dist_mean))
-
     plt.ylabel('Probability')
     plt.xlabel('Bias')
     plt.legend(('Dynamic', 'Static', 'Dynamic', 'Static'), loc='upper left')
     print(scipy.stats.ttest_ind(bias_distribution_best_nn, bias_distribution_naive_optimal_static))
-    plt.figure()
-    fig, ax = plt.subplots()
-    ax.plot(generation_mean_bias[0], generation_mean_bias[1])
-    ax.set(xlabel='Generation', ylabel='Mean Generation Bias (%)')
-    ax.grid()
-
-    fig.savefig("test.png")
-
     plt.show()
+    return bias_distribution_naive_optimal_static
 
 
 if __name__ == '__main__':